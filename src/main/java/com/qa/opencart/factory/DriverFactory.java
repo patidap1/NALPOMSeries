71c1d2629038361ab2dd555b90987e6aba0ea2b5
Pawan Patidar <patidarpawan1986@gmail.com>
2025-03-03 03:27:59 +0530

    Enabled remote grid based executions

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
@@ -15,6 +17,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 import com.qa.opencart.constants.AppConstants;
@@ -30,38 +33,52 @@ public class DriverFactory {
 	public static String highlight;
 
 	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<WebDriver>();
-    private static final Logger log = LogManager.getLogger(DriverFactory.class);
-	
+	private static final Logger log = LogManager.getLogger(DriverFactory.class);
+
 	@Step("init the driver using properties : {0}")
 	public WebDriver initDriver(Properties prop) {
-
 		String browserName = prop.getProperty("browser");
-		//System.out.println("browser name is :" + browserName);
-		log.info("browser name is : "+browserName);
+		// System.out.println("browser name is :" + browserName);
+		log.info("browser name is : " + browserName);
 
 		highlight = prop.getProperty("highlight");
 		optionsManager = new OptionsManager(prop);
-		
+
+		boolean remoteExecution = Boolean.parseBoolean(prop.getProperty("remote"));
+
 		switch (browserName.trim().toLowerCase()) {
 		case "chrome":
-			tlDriver.set(new ChromeDriver(optionsManager.getChromeOptions()));
-			// driver = new ChromeDriver(optionsManager.getChromeOptions());
+			if (remoteExecution) {
+				// run tcs on remote/grid:
+				initRemoteDriver("chrome");
+			} else {
+				// local execution:
+				tlDriver.set(new ChromeDriver(optionsManager.getChromeOptions()));
+			}
 			break;
 		case "firefox":
-			tlDriver.set(new FirefoxDriver(optionsManager.getFirefoxOptions()));
-			// driver = new FirefoxDriver(optionsManager.getFirefoxOptions());
+			if (remoteExecution) {
+				// run tcs on remote/grid:
+				initRemoteDriver("firefox");
+			} else {
+				// local execution:
+				tlDriver.set(new FirefoxDriver(optionsManager.getFirefoxOptions()));
+			}
 			break;
 		case "edge":
-			tlDriver.set(new EdgeDriver(optionsManager.getEdgeOptions()));
-			// driver = new EdgeDriver(optionsManager.getEdgeOptions());
+			if (remoteExecution) {
+				// run tcs on remote/grid:
+				initRemoteDriver("edge");
+			} else {
+				// local execution:
+				tlDriver.set(new EdgeDriver(optionsManager.getEdgeOptions()));
+			}
 			break;
 		case "safari":
 			tlDriver.set(new SafariDriver());
-			// driver = new SafariDriver();
 			break;
 		default:
-			//System.out.println("plz pass the valid browser name.." + browserName);
-			log.error("plz pass the valid browser name.."+ browserName);
+			log.error("plz pass the valid browser name.." + browserName);
 			throw new FrameworkException("===invalid browser name===");
 		}
 
@@ -70,6 +87,37 @@ public class DriverFactory {
 		getDriver().get(prop.getProperty("url"));
 		return getDriver();
 
+	}
+
+	/**
+	 * init the remote driver with selenium grid
+	 * 
+	 * @param browserName
+	 */
+	private void initRemoteDriver(String browserName) {
+		System.out.println("Running tests on grid: " + browserName);
+
+		try {
+			switch (browserName.toLowerCase().trim()) {
+			case "chrome":
+				tlDriver.set(
+						new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsManager.getChromeOptions()));
+				break;
+			case "firefox":
+				tlDriver.set(
+						new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsManager.getFirefoxOptions()));
+				break;
+			case "edge":
+				tlDriver.set(new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsManager.getEdgeOptions()));
+				break;
+
+			default:
+				System.out.println("browser is not supported on GRID... " + browserName);
+				break;
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -90,14 +138,14 @@ public class DriverFactory {
 	// mvn clean install
 	public Properties initProp() {
 		String envName = System.getProperty("env");
-		//System.out.println("running test suite on env: " + envName);
+		// System.out.println("running test suite on env: " + envName);
 		log.info("running test suite on env: " + envName);
 		FileInputStream ip = null;
 		prop = new Properties();
-		
+
 		try {
 			if (envName == null) {
-				//System.out.println("no env is passed, hence running test suite on qa env..");
+				// System.out.println("no env is passed, hence running test suite on qa env..");
 				log.warn("no env is passed, hence running test suite on qa env..");
 				ip = new FileInputStream(AppConstants.CONFIG_QA_PROP_FILE_PATH);
 			} else {
@@ -119,7 +167,7 @@ public class DriverFactory {
 					break;
 
 				default:
-					//System.out.println("plz pass the right env name..."+ envName);
+					// System.out.println("plz pass the right env name..."+ envName);
 					log.error("plz pass the right env name..." + envName);
 					throw new FrameworkException("===INVALID ENV===");
 				}
@@ -136,17 +184,16 @@ public class DriverFactory {
 
 		return prop;
 	}
-	
-	
+
 	/**
 	 * takescreenshot
 	 */
-	
+
 	public static String getScreenshot() {
 		File srcFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);// temp dir
-		String path = System.getProperty("user.dir") + "/screenshot/"  + "_" + System.currentTimeMillis()+ ".png";		
+		String path = System.getProperty("user.dir") + "/screenshot/" + "_" + System.currentTimeMillis() + ".png";
 		File destination = new File(path);
-		
+
 		try {
 			FileHandler.copy(srcFile, destination);
 		} catch (IOException e) {
@@ -155,24 +202,20 @@ public class DriverFactory {
 
 		return path;
 	}
-	
+
 	public static File getScreenshotFile() {
 		File srcFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);// temp dir
 		return srcFile;
 	}
-	
+
 	public static byte[] getScreenshotByte() {
 		return ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES);// temp dir
-		
+
 	}
-	
+
 	public static String getScreenshotBase64() {
 		return ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BASE64);// temp dir
-		
+
 	}
-	
-	
-	
-	
 
 }
--- src/main/java/com/qa/opencart/factory/OptionsManager.java
+++ src/main/java/com/qa/opencart/factory/OptionsManager.java
@@ -1,5 +1,7 @@
 package com.qa.opencart.factory;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
@@ -14,9 +16,8 @@ public class OptionsManager {
 	private ChromeOptions co;
 	private FirefoxOptions fo;
 	private EdgeOptions eo;
-	
-    private static final Logger log = LogManager.getLogger(OptionsManager.class);
 
+	private static final Logger log = LogManager.getLogger(OptionsManager.class);
 
 	public OptionsManager(Properties prop) {
 		this.prop = prop;
@@ -25,14 +26,25 @@ public class OptionsManager {
 	public ChromeOptions getChromeOptions() {
 		co = new ChromeOptions();
 		if (Boolean.parseBoolean(prop.getProperty("headless"))) {
-			//System.out.println("==Running in HeadLess Mode===");
+			// System.out.println("==Running in HeadLess Mode===");
 			log.info("==Running in HeadLess Mode===");
 			co.addArguments("--headless");
 		}
 		if (Boolean.parseBoolean(prop.getProperty("incognito"))) {
-			//System.out.println("==Running in Incognito Mode===");
+			// System.out.println("==Running in Incognito Mode===");
 			log.info("==Running in Incognito Mode===");
 			co.addArguments("--incognito");
+		}
+		if (Boolean.parseBoolean(prop.getProperty("remote"))) {
+			co.setCapability("browserName", "chrome");
+			co.setBrowserVersion(prop.getProperty("browserversion").trim());
+
+			Map<String, Object> selenoidOptions = new HashMap<>();
+			selenoidOptions.put("screenResolution", "1280x1024x24");
+			selenoidOptions.put("enableVNC", true);
+			selenoidOptions.put("name", prop.getProperty("testname"));
+			co.setCapability("selenoid:options", selenoidOptions);
+
 		}
 		return co;
 	}
@@ -40,14 +52,22 @@ public class OptionsManager {
 	public FirefoxOptions getFirefoxOptions() {
 		fo = new FirefoxOptions();
 		if (Boolean.parseBoolean(prop.getProperty("headless"))) {
-			// System.out.println("==Running in HeadLess Mode===");
-			log.info("==Running in HeadLess Mode===");
+			System.out.println("==Running in HeadLess Mode===");
 			fo.addArguments("--headless");
 		}
 		if (Boolean.parseBoolean(prop.getProperty("incognito"))) {
-			// System.out.println("==Running in Incognito Mode===");
-			log.info("==Running in Incognito Mode===");
+			System.out.println("==Running in Incognito Mode===");
 			fo.addArguments("--incognito");
+		}
+		if (Boolean.parseBoolean(prop.getProperty("remote"))) {
+			fo.setCapability("browserName", "firefox");
+			fo.setBrowserVersion(prop.getProperty("browserversion").trim());
+
+			Map<String, Object> selenoidOptions = new HashMap<>();
+			selenoidOptions.put("screenResolution", "1280x1024x24");
+			selenoidOptions.put("enableVNC", true);
+			selenoidOptions.put("name", prop.getProperty("testname"));
+			fo.setCapability("selenoid:options", selenoidOptions);
 		}
 		return fo;
 	}
@@ -55,14 +75,15 @@ public class OptionsManager {
 	public EdgeOptions getEdgeOptions() {
 		eo = new EdgeOptions();
 		if (Boolean.parseBoolean(prop.getProperty("headless"))) {
-			// System.out.println("==Running in HeadLess Mode===");
-			log.info("==Running in Headless Mode===");
+			System.out.println("==Running in HeadLess Mode===");
 			eo.addArguments("--headless");
 		}
 		if (Boolean.parseBoolean(prop.getProperty("incognito"))) {
-			// System.out.println("==Running in Incognito Mode===");
-			log.info("==Running in Incognito Mode===");
+			System.out.println("==Running in Incognito Mode===");
 			eo.addArguments("--inprivate");
+		}
+		if (Boolean.parseBoolean(prop.getProperty("remote"))) {
+			eo.setCapability("browserName", "edge");
 		}
 		return eo;
 	}
